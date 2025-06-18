4ba4f7fea5d72cd11d4a4dce5f1f9f7a466f39e8
TechPortalTaem <tech.portal.taem@gmail.com>
2025-06-18 08:55:33 +0200

    commit
    commit 2 tag 1 erste

--- src/data/profList.js
+++ src/data/profList.js
@@ -1,18 +1,21 @@
-export const profList= [
+ export const profList =[
 {
-id: 1,
-number: "3+",
-text: "Years of experience",
-},
-{
-id: 2,
-number: "4K+",
-text: "Happy Customers",
-},
-
-{
-id: 3,
-number: "3+",
-text: "Success Projects",
-}
+    id:1,
+    number: "3+",
+    text:"Years of experience"
+    
+ },
+ {
+    id:2,
+    number: "4K+",
+    text:"Happy Customers"
+    
+ },
+ {
+    id:3,
+    number: "3+",
+    text:"Success Projects"
+    
+ }
+ 
 ]
